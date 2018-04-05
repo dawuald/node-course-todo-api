1d791bb5e5ca6dfb64c1fd0906b42ec5ff682df6
Dawid Bijak <bijak.dawid@gmail.com>
2018-04-05 20:17:00 +0200

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -31,6 +31,7 @@ var UserSchema = new mongoose.Schema({
         }
     }]
 });
+
 UserSchema.methods.toJSON = function(){
     var user = this;
     var userObject = user.toObject();
@@ -46,8 +47,25 @@ UserSchema.methods.generateAuthToken = function(){
 
     return user.save().then(() => {
         return token;
-    })
+    });
 };
+
+UserSchema.statics.findByToken = function(token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch(e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+}
 
 var User = mongoose.model('User', UserSchema);
 
