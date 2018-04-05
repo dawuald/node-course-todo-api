481c135ba10bda59fbd9b557a597dd2b0095aefd
Dawid Bijak <bijak.dawid@gmail.com>
2018-04-05 09:45:31 +0200

    Setup for heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const {User} = require('./models/user');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -46,8 +48,8 @@ app.get('/todos/:id', (req, res) => {
     });;
 });
 
-app.listen(3000, () => {
-    console.log('Express running...');
+app.listen(port, () => {
+    console.log(`Express runningo on ${port}...`);
 });
 
 module.exports = {
